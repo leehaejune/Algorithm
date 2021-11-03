e3c2ff2d6836780c7c4c5489f8b1e8d248e31540
leehaejune <57649614+leehaejune@users.noreply.github.com>
2021-11-03 22:32:03 +0900

    백준 8단계 -04

--- algorithm/BAEKJOON/단계별/practice08/practice08.java
+++ algorithm/BAEKJOON/단계별/practice08/practice08.java
@@ -71,6 +71,22 @@ public class Practice08 {
         }    
       }
     }
+    public void practice08_4(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+    
+        int day = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0)
+            day++;
+    
+        System.out.println(day);
+}
+    //시간초과되서 bufferedReader 사용
+}
     //분자 자리 분모자리 확인
     //X값을 받음, (n)
     //짝수 1/2 > 2/1 
